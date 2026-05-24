1525087bd568d56d0de90168e39b2faa4433323d
joytivism <joytivism@gmail.com>
2026-05-24 14:31:03 +0700

    fix: update .gitignore, remove .next and build artifacts

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Volumes/JOY/PAKARSHEET LP/app/layout\":[\"static/media/b5bb120f6fcabcb4-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
